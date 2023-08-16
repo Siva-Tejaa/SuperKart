f3ecb3ec996a9610ea0d035598897626a63b4c3d
Siva-Tejaa <asivateja1999@gmail.com>
2023-08-16 12:35:50 +0530

    modified cart page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -11,33 +11,25 @@ const Cart = () => {
     <div className="cart">
       <div className="cart-left">
         <div className="cart-title">
-          <div className="cart-title-main">
-            Shopping Cart
-          </div>
-          <div className="clear-cart">
-            Clear Cart
-          </div>
+          <div className="cart-title-main">Shopping Cart</div>
+          <div className="clear-cart">Clear Cart</div>
         </div>
-        {
-          data.map((cartproduct, index) => (
-            <div className="cart-products" key={cartproduct?.id}>
-              <div className="cart-product-image">
-                <img src={cartproduct?.thumbnail}/>
-              </div>
-
-              <div className="cart-product-details">
-
-              </div>
+        {data.map((cartproduct, index) => (
+          <div className="cart-products" key={cartproduct?.id}>
+            <div className="cart-product-image">
+              <img src={cartproduct?.images[1]} className="cart-prod-image" />
             </div>
-          ))
-        }        
+
+            <div className="cart-product-details">
+              <div className="cart-product-title">{cartproduct?.title}</div>
+            </div>
+          </div>
+        ))}
       </div>
       <div className="cart-right">
         <p>Order Summary</p>
-      {JSON.stringify(data)}
-
+        {JSON.stringify(data)}
       </div>
-
     </div>
   );
 };
