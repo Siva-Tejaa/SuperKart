a630088760f7f40a69df9da08255289d85ef1011
Siva-Tejaa <asivateja1999@gmail.com>
2023-09-04 20:28:31 +0530

    modified cart items ui

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "./Cart.css";
+
 import { useSelector, useDispatch } from "react-redux";
-import { clearCart } from "../../redux/features/cartSlice";
+import {
+  incr1,
+  decr1,
+  delprod,
+  clearCart,
+} from "../../redux/features/cartSlice";
 
 import toast, { Toaster } from "react-hot-toast";
 
@@ -14,6 +20,20 @@ const Cart = () => {
     dispatch(clearCart());
   };
 
+  const shareHandler = (data) => {
+    const sharedData = {
+      name: data?.title,
+      price: data?.price,
+      image: data?.images[0],
+      description: data?.description,
+    };
+    if (navigator.canShare && navigator.canShare(sharedData)) {
+      navigator.canShare(sharedData);
+    } else {
+      console.log("Sharing Not Supported");
+    }
+  };
+
   return (
     <div className="cart">
       <div className="cart-left">
@@ -24,32 +44,99 @@ const Cart = () => {
           </div>
         </div>
 
-        {data.map((cartproduct, index) => (
-          <div className="cart-products" key={cartproduct?.id}>
-            <div className="cart-product-image">
-              <img src={cartproduct?.images[0]} className="cart-prod-image" />
-            </div>
+        {data?.length > 0 ? (
+          <>
+            {data.map((cartproduct, index) => (
+              <div className="cart-products" key={cartproduct?.id}>
+                <div className="cart-product-image">
+                  <img
+                    src={cartproduct?.images[0]}
+                    className="cart-prod-image"
+                  />
+                </div>
 
-            <div className="cart-product-details">
-              <div className="cart-product-title">{cartproduct?.title}</div>
-              <div className="cart-price-section">
-              
-               <h4>₹{cartproduct?.price}</h4>
-               <del className='prod-price'>₹{cartproduct?.originalprice}</del>
-               <div className='product-off'>{Math.ceil(cartproduct?.discountPercentage)}%off</div>
+                <div className="cart-product-details">
+                  <div className="cart-product-title">{cartproduct?.title}</div>
+                  <div className="cart-price-section">
+                    <h4>₹{cartproduct?.price}</h4>
+                    <del className="prod-price">
+                      ₹{cartproduct?.originalprice}
+                    </del>
+                    <div className="product-off">
+                      {Math.ceil(cartproduct?.discountPercentage)}%off
+                    </div>
+                  </div>
+                  <div>
+                    Category :{" "}
+                    {cartproduct?.category?.charAt(0).toUpperCase() +
+                      cartproduct?.category?.slice(1)}
+                  </div>
+                  <div className="checkout-qty-parent">
+                    <button
+                      className="checkout-btn"
+                      onClick={() => dispatch(decr1(cartproduct))}
+                    >
+                      -
+                    </button>
+                    <input
+                      className="checkout-quantity"
+                      type="number"
+                      // min="1"
+                      // max={data?.stock > 0 && `${data?.stock}`}
+                      disabled
+                      value={cartproduct?.quantity}
+                    />
+                    <button
+                      className="checkout-btn"
+                      onClick={() => dispatch(incr1(cartproduct))}
+                    >
+                      +
+                    </button>
+                  </div>
+                  <div className="cart-delete-share">
+                    <button
+                      className="cart-delete"
+                      onClick={() => dispatch(delprod(cartproduct))}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      className="cart-share"
+                      onClick={() => shareHandler(cartproduct)}
+                    >
+                      Share
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div>
-                Category :{" "}
-                {cartproduct?.category?.charAt(0).toUpperCase() +
-                  cartproduct?.category?.slice(1)}
-              </div>
-            </div>
-          </div>
-        ))}
+            ))}
+          </>
+        ) : (
+          <>
+            <img src="https://www.ipack.com/media/icons/empty-cart-ipack.png" />
+          </>
+        )}
       </div>
       <div className="cart-right">
-        <p>Order Summary</p>
-        {JSON.stringify(data)}
+        <p className="cart-order-summary">Order Summary</p>
+        {/* {JSON.stringify(data)} */}
+        <div className="cart-order">
+          <p>
+            Price ({data?.length} {data?.length > 1 ? "items" : "item"})
+          </p>
+          <p>₹ 555</p>
+        </div>
+        <div className="cart-order">
+          <p>Delivery</p>
+          <p>FREE</p>
+        </div>
+        <hr />
+
+        <div className="cart-order">
+          <p>Amount Payable</p>
+          <p>₹ 999</p>
+        </div>
+        <button className="cart-checkout-button">CheckOut</button>
       </div>
     </div>
   );
--- src/redux/features/cartSlice.jsx
+++ src/redux/features/cartSlice.jsx
@@ -15,12 +15,53 @@ export const categoriesSlice = createSlice({
         state.unshift(action.payload);
       }
     },
+    incr1: (state, action) => {
+      const productExist = state.find((prod) => prod.id === action.payload.id);
+
+      const maxQty = action.payload?.stock > 0 && `${action.payload?.stock}`;
+
+      // console.log(maxQty, productExist.quantity);
+
+      if (productExist.quantity == maxQty) {
+        // errorSound.play();
+        // toast.error("Product Stock Not Available!");
+        return;
+      }
+
+      if (productExist) {
+        productExist.quantity = productExist.quantity + 1;
+      }
+    },
+    decr1: (state, action) => {
+      const productExist = state.find((prod) => prod.id === action.payload.id);
+      const productExistIndex = state.findIndex(
+        (prod) => prod.id === action.payload.id
+      );
+
+      if (productExist.quantity == 1) {
+        // productExist.quantity = productExist.quantity - 1;
+        // console.log(action.payload);
+        // console.log(state.filter((prod) => prod.id != action.payload.id));
+
+        state.splice(productExistIndex, 1);
+      } else {
+        productExist.quantity = productExist.quantity - 1;
+      }
+    },
+    delprod: (state, action) => {
+      const productExistIndex = state.findIndex(
+        (prod) => prod.id === action.payload.id
+      );
+
+      state.splice(productExistIndex, 1);
+    },
     clearCart: (state) => {
       state.length = 0;
     },
   },
 });
 
-export const { cart, clearCart } = categoriesSlice.actions;
+export const { cart, incr1, decr1, delprod, clearCart } =
+  categoriesSlice.actions;
 
 export default categoriesSlice.reducer;
